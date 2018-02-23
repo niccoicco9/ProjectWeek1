13b4f8cf9612a2cba0d8df0cf144683b3775bed5
Alessandro Corrado <ale.corrado91@gmail.com>
2018-02-23 10:03:35 +0100

    test del post su fake api server

--- scriptContact.js
+++ scriptContact.js
@@ -1,8 +1,38 @@
 /* exported loadInformation */
 /* global loadHeaderInformation, $*/
+$('body').loading({
+    onStart: function(loading) {
+        'use strict';
+        loading.overlay.slideDown(400);
+    },
+    onStop: function(loading) {
+        'use strict';
+        loading.overlay.slideUp(400);
+    },
+    theme: 'dark'
+  });
+
 
 function loadInformation(){
     'use strict';
     setTimeout($('body').loading('toggle'),2000);
     loadHeaderInformation();
+}
+
+
+
+function submitPost(){
+    'use strict';
+    var persona = {
+        "name": {
+            "title": "mr",
+            "first": "Dries",
+            "last": "Mertens"
+        }
+    };
+
+    $.post("http://localhost:3000/results", persona).done(function(data){
+        alert("Dati caricati: " + data);
+    });
+
 }
